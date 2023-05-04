34143e785a2aca8cf32e41e7b27c1ca6500ce42a
Epharus Ndumia <epharusndumia@gmail.com>
2023-05-04 17:55:21 +0300

    styled the Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>App created by @Epharus</div>;
+  return (
+    <div className="container-fluid d-flex justify-content-center text-align-center text-white bg-dark p-3">
+      <h4>App created by @Epharus</h4>
+    </div>
+  );
 };
 
 export default Footer;
